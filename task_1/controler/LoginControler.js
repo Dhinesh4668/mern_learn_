433ba9482418535b7667a450a46f6a17dfc0431e
Dhinesh4668 <dhinesh4668@gmail.com>
2024-01-12 13:25:35 +0530

    l

--- task_1/controler/LoginControler.js
+++ task_1/controler/LoginControler.js
@@ -3,15 +3,17 @@ const express = require('express');
 const user = require('../modals/userModal');
 const jwt = require('jsonwebtoken');
 
+
 const loginUser = async (req, res) => {
     try {
         // Validate the user
-        const { username, password } = req.body;
-        const loginUser = user.find(username === username && password === password);
+        const { email,username, password } = req.body;
+        const loginUser = user.find((username === username ||username === email) && password === password)
 
         if (loginUser) {
-            const token = jwt.sign({ username: loginUser.username }, process.env.SECRET_KEY);
-            res.json({ token });
+            const token = jwt.sign({ username: loginUser.username , password: loginUser.password}, process.env.SECRET_KEY);
+            const RefreshTocken = jwt.sign({ username: loginUser.username , password: loginUser.password}, process.env.REFRESH_KEY);
+            res.json({ token, RefreshTocken });
         } else {
             res.status(401).json({ error: "Invalid credentials" });
         }
