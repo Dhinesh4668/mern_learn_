26ae6e2032ea9ccd5dc7d8cad1ac68260d057a39
Dhinesh4668 <dhinesh4668@gmail.com>
2024-01-25 10:00:49 +0530

    task3 server login token  setup complete

--- task_3/backend/controllers/userLogin.js
+++ task_3/backend/controllers/userLogin.js
@@ -1,16 +1,53 @@
-const user = require("../models/userModel");
+require('dotenv').config();
+const userModel = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const bcrypt = require('bcryptjs')
+
 
 const login = async (req, res) => {
-  const { email, passsword } = req.body;
   try {
-    const isemail = await user.findOne({ email });
-    if (!isemail) {
-        return res.status(404).json({ error: `User not found "${UserName}." Please check your email.` });
-    } else {
-      res.send("login sucess");
+    const { email, password } = req.body;
+    const Salt = await bcrypt.genSaltSync(10)
+    const encrypt_Pass = await bcrypt.hashSync(password, Salt)
+    // Check if the user with the provided email exists
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({
+        error: `User not found. Please check your email.`,
+      });
     }
+
+    // Compare passwords
+    const isPasswordValid = await bcrypt.compareSync(password, encrypt_Pass);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        error: "Invalid password",
+      });
+    }
+
+    // Generate JWT tokens
+    const token = jwt.sign(
+      { userId: user._id, email: user.email },
+      process.env.SECRET_KEY,
+      { expiresIn: "30d" }
+    );
+    const refreshToken = jwt.sign(
+      { userId: user._id, email: user.email },
+      process.env.REFRESH_KEY,
+      { expiresIn: "30d" }
+    );
+
+    // Send response with tokens and user data
+    res.status(200).json({
+      data: { userId: user._id, email: user.email },
+      token,
+      refreshToken,
+    });
   } catch (err) {
-    console.log(err.message);
+    console.error(err.message);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
